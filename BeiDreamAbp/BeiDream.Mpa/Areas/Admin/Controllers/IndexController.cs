1d6764e0b1dcc51634764f994fb26a85a1c90f20
BeiDream <331341164@qq.com>
2016-05-31 22:15:57 +0800

    添加后台管理页面模板文件

---
+++ BeiDreamAbp/BeiDream.Mpa/Areas/Admin/Controllers/IndexController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeiDream.Mpa.Areas.Admin.Controllers
+{
+    public class IndexController : Controller
+    {
+        // GET: Admin/Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
