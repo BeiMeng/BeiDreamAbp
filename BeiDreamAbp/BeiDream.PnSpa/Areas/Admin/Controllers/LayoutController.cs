397d9b6d217ae2fe5901515ab7f64554bb8ebd5e
BeiDream <331341164@qq.com>
2016-06-02 17:30:39 +0800

    伪Spa模板添加

---
+++ BeiDreamAbp/BeiDream.PnSpa/Areas/Admin/Controllers/LayoutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BeiDream.PnSpa.Areas.Admin.Controllers
+{
+    public class LayoutController : Controller
+    {
+        // GET: Admin/Layout
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
