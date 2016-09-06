be41dc5cf97499adad2ebd0b0498c8f6df11dca5
BeiDrean <331341164@qq.com>
2016-09-06 15:49:17 +0800

    添加登录功能

---
+++ BeiDreamAbp/BeiDream.MetronicMpa/App_Start/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+// Microsoft.Owin.Host.SystemWeb,OWIN服务器实现,它允许基于OWIN的应用程序运行在IIS中
+//，并能够使用ASP.NET的请求管道(引用此dll,才能程序启动时执行下面的代码)
+[assembly: OwinStartup(typeof(BeiDream.MetronicMpa.Startup))]
+
+namespace BeiDream.MetronicMpa
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Admin/Account/Login")
+            });
+        }
+    }
+}
--- BeiDreamAbp/BeiDream.MetronicMpa/Areas/Admin/Controllers/DashboardController.cs
+++ BeiDreamAbp/BeiDream.MetronicMpa/Areas/Admin/Controllers/DashboardController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Web.Mvc;
+using Abp.Web.Mvc.Authorization;
 
 namespace BeiDream.MetronicMpa.Areas.Admin.Controllers
 {
+    [AbpMvcAuthorize]
     public class DashboardController : AdminController
     {
         // GET: Admin/Index
